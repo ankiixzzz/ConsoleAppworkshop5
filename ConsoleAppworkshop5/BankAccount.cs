698fc9eb8c5c270f896c313eb9c9a5497e5ba866
ankiixzzz <rautankita348@gmail.com>
2025-11-26 11:25:58 +0545

    Add project files.

---
+++ ConsoleAppworkshop5/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿namespace DotnetWorkshop
+{
+    public class BankAccount
+    {
+        // Private fields
+        private string accountNumber;
+        private double balance;
+
+        // Public property - AccountNumber (Only Get)
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        // Public property - Balance (Get only, private set)
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value > 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Balance cannot be zero or negative.");
+                }
+            }
+        }
+
+        // Constructor to set account number and initial balance
+        public BankAccount(string accNo, double initialBalance)
+        {
+            accountNumber = accNo;
+            Balance = initialBalance;  // uses the property setter
+        }
+
+        // Deposit Method
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be greater than 0");
+            }
+        }
+
+        // Withdraw Method
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdraw amount must be greater than 0");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Insufficient balance!");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount}");
+            }
+        }
+    }
+}
---
+++ ConsoleAppworkshop5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DotnetWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAcc = new BankAccount("ACC12345", 5000);
+
+            Console.WriteLine("Account Number: " + myAcc.AccountNumber);
+            Console.WriteLine("Initial Balance: " + myAcc.Balance);
+
+            // Deposit money
+            myAcc.Deposit(2000);
+            Console.WriteLine("Balance after deposit: " + myAcc.Balance);
+
+            // Withdraw money
+            myAcc.Withdraw(1500);
+            Console.WriteLine("Balance after withdrawal: " + myAcc.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
